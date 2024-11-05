af6908bb978bfe3d928f92f45de530355de7713b
riazahmedmahin <c201060@ugrad.iiuc.ac.bd>
2024-11-05 20:03:43 +0600

    commit

---
+++ services/models/InvoiceModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const DataSchema= new mongoose.Schema(
+    {
+        user_ID:{type: mongoose.Schema.Types.ObjectId,required:true},
+        payable:{type: String,required: true},
+        cus_details:{type: String,required: true},
+        ship_details:{type: String,required: true},
+        train_id:{type: String,required: true},
+        val_id:{type: String,required: true},
+        delivery_status:{type: String,required: true},
+        payment_status:{type: String,required: true},
+        total:{type: String,required: true},
+        vat:{type: String,required: true},
+        
+    },
+    {
+        timestamps:true,
+        versionKey:false
+    }
+)
+
+const InvoiceModel = mongoose.model('invoices',DataSchema)
+
+export default InvoiceModel;
